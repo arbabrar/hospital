74097b2c45b2c907da0bc8ed97b56b1527127f91
FerBoss1 <www.fernandomf@gmail.com>
2025-02-21 22:54:32 -0400

    Actualizacion de paquetes y correccion de errores

---
+++ storage/framework/views/88578ea1e63429b65c6439c925d512fc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hospital UPDS | 2025</title>
+   
+    <?php echo app('Illuminate\Foundation\Vite')("resources/js/app.js"); ?>
+</head>
+<body>
+    <div id="root">
+
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\SisInfo3\hospital\resources\views/welcome.blade.php ENDPATH**/ ?>
